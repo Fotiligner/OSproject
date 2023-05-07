4c8768f02c5631838e425693bef9488f6232f688
Nauhc-C <76831634+Nauhc-C@users.noreply.github.com>
2023-05-07 19:01:13 +0800

    制作kill正在运行的进程, 修复kill的bug

--- UI/Process_Module_UI.py
+++ UI/Process_Module_UI.py
@@ -306,7 +306,19 @@ class createProcessLabel(QLabel):
         layout.addWidget(self.text_label)
         layout.addWidget(self.text_label2)
 
+        self.buttonKill = QPushButton("kill当前进程")
+        layout.addWidget(self.buttonKill)
+        self.buttonKill.clicked.connect(lambda: self.killProcess())
         self.setLayout(layout)
+
+    def killProcess(self):
+        if(self.process_module.running_pid!=-1):
+            try:
+                self.process_module.kill_process_safe(self.process_module.running_pid)
+            except Exception as ex:
+                print("出现如下异常%s" % ex)
+
+
     def updateText(self):
         # 获取值
         try:
